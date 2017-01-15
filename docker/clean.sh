c0d7b825e1c7c8a55dfbff05339f7bc7aa26b98e
Raphael Michel <mail@raphaelmichel.de>
2017-01-15 19:30:01 +0100

    Remove dangling volumes

--- docker/clean.sh
+++ docker/clean.sh
@@ -3,4 +3,5 @@ sudo /usr/bin/supervisord
 sleep 3
 docker rm --volumes $(docker ps -q -f status=exited)
 docker rmi $(docker images -f "dangling=true" -q)
+docker volume ls -qf dangling=true | xargs -r docker volume rm
 
