c498ea1f2ab46dd72c0a58346d3b1669026476ae
Raphael Michel <mail@raphaelmichel.de>
2017-01-20 11:02:37 +0100

    Docker 1.13

--- docker/clean.sh
+++ docker/clean.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 sudo /usr/bin/supervisord
 sleep 3
-docker rm --volumes $(docker ps -q -f status=exited)
-docker rmi $(docker images -f "dangling=true" -q)
-docker volume ls -qf dangling=true | xargs -r docker volume rm
+docker system prune -f
 
