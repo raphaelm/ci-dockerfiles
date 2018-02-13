3bc1ef9cf5ec64b3462a96efe9e2b62b8cabd285
Raphael Michel <mail@raphaelmichel.de>
2018-02-13 10:10:17 +0100

    Update clean.sh to wipe even more things

--- docker/clean.sh
+++ docker/clean.sh
@@ -3,3 +3,6 @@ sudo /usr/bin/supervisord
 sleep 3
 docker system prune -f
 
+docker ps -q -f status=exited | xargs -r docker rm --volumes
+docker images -q --filter "dangling=true" | xargs -r docker rmi
+docker volume ls -qf dangling=true | xargs -r docker volume rm
