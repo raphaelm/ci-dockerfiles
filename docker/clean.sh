9561efdf13c065a1bc57153484ab4dc669f401de
Raphael Michel <mail@raphaelmichel.de>
2021-12-02 11:34:58 +0100

    Fix docker clean script

--- docker/clean.sh
+++ docker/clean.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
-sudo /usr/bin/supervisord
-sleep 3
+#!/bin/bash
+/usr/bin/supervisord
+for i in {0..10};
+do
+	sleep 2;
+	docker info && break;
+done
 docker system prune -f
-
 docker ps -q -f status=exited | xargs -r docker rm --volumes
 docker images -q --filter "dangling=true" | xargs -r docker rmi
 docker volume ls -qf dangling=true | xargs -r docker volume rm
